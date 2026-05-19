c9b410601dee48e8394c2d6dad772c1652e34f96
Lauretta Maria Gullì <info@educareweb.it>
2026-05-19 11:48:54 +0200

    Allow role users to access Filament panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -68,5 +70,10 @@ class User extends Authenticatable
     public function isStoreUser(): bool
     {
         return $this->role === 'store';
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return in_array($this->role, ['super_admin', 'admin', 'store'], true);
     }
 }
